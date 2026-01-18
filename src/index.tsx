b50fd972702555f9f0708eeabafd98e4703e318f
captainburbseye-web <genspark_dev@genspark.ai>
2026-01-18 12:19:31 +0000

    Revert "Apply absolute positioning to street sign - eliminate dead space from flex layout"
    This reverts commit 4d184619ecf36320c41310690226db453021e833.

--- src/index.tsx
+++ src/index.tsx
@@ -338,7 +338,7 @@ const Header = () => (
         </nav>
       </div>
       
-      {/* ABSOLUTE POSITIONED: Street Sign on Far Right */}
+      {/* RIGHT ZONE: Street Sign */}
       <div class="rack-header-right">
         <img 
           src="https://pub-991d8d2677374c528678829280f50c98.r2.dev/crs-images%20website/512%20CRS%20Title%20Road%20Sign.png"
