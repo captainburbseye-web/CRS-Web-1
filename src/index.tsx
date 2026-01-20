cef681589b3f0e3c5da1410166eb886eb990bebf
captainburbseye-web <genspark_dev@genspark.ai>
2026-01-20 10:54:15 +0000

    Logo swap: CRS Title Road Sign replaces badge+wordmark lockup

--- src/index.tsx
+++ src/index.tsx
@@ -313,7 +313,7 @@ const Header = () => (
       <div class="rack-header-left">
         <a href="/" style="display: block; line-height: 0;">
           <img 
-            src="https://pub-991d8d2677374c528678829280f50c98.r2.dev/x2%201024%20BADGE%20%2B%20WORDMARK%20LOCKUP.png" 
+            src="https://pub-991d8d2677374c528678829280f50c98.r2.dev/crs-images%20website/CRS%20Title%20Road%20Sign.png" 
             alt="Cowley Road Studios"
             class="crs-main-logo"
           />
