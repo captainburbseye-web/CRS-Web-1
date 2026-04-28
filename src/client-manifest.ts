5405ab872930d8396d1bdce9e2bed0da1a153456
captainburbseye-web <genspark_dev@genspark.ai>
2026-04-28 22:58:49 +0000

    feat: increase CRS rack scale and impact (v3.2)

--- src/client-manifest.ts
+++ src/client-manifest.ts
@@ -2,12 +2,12 @@
 // This file is generated during the build process
 export const CLIENT_MANIFEST = {
   "src/client/rack-entry.tsx": {
-    "file": "assets/rack-entry-CR9doNXm.js",
+    "file": "assets/rack-entry-DJxFEShF.js",
     "name": "rack-entry",
     "src": "src/client/rack-entry.tsx",
     "isEntry": true,
     "css": [
-      "assets/rack-entry-C-tMWJFT.css"
+      "assets/rack-entry-CS_bWuxF.css"
     ]
   }
 } as const
